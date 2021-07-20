dfd0a5cc9fc2bc772552b1757ef364b7eaab4e19
Ken30116 <ken141718@gmail.com>
2021-07-20 18:10:44 +0800

    p

--- javascript/header-nav.js
+++ javascript/header-nav.js
@@ -1,5 +1,6 @@
 window.onscroll = function () {
     let t = document.documentElement.scrollTop || document.body.scrollTop;
+    let winheight = window.innerHeight;
     let logo = document.getElementById("logo");
     let header = document.getElementsByTagName("header");
     let item = document.getElementsByClassName("item");
@@ -9,10 +10,29 @@ window.onscroll = function () {
     let slideshow2 = document.getElementsByClassName("slideshow");
     let historyshow= document.getElementsByClassName("history");
     let msg=document.getElementsByClassName("history-msg");
+    let clickshow=document.getElementById("clickshow");
+    let history_box=document.getElementsByClassName("history_box");
+    let historyxy=history_box[0].getBoundingClientRect();
+    let newimg=document.getElementsByClassName("new_img-click");
+    let newimgxy= newimg[0].getBoundingClientRect();
+    let smallimg=document.getElementsByClassName("small_img");
+    let newmsg=document.getElementsByClassName("new-msg");
 
-
-
+    if (newimgxy.top>0 && newimgxy.bottom<winheight) {
+        for (let i = 0; i < smallimg.length; i++) {
+            smallimg[i].style.display="block";
+        }
+        newmsg[0].style.display="block";
+    }
+    else {
+        for (let i = 0; i < smallimg.length; i++) {
+            smallimg[i].style.display="none";
+        }
+        newmsg[0].style.display="none";
+    }
+    
     if (mq.matches) {
+        clickshow.src="./img/home/mouse-click.png"
         if (t >= 300) {
             logo.style.width = "0%";
             logo.style.minWidth="0";
@@ -29,16 +49,17 @@ window.onscroll = function () {
             item[1].style.marginRight = "11em";
             slideshow2[0].style.clipPath="polygon(100% 0, 100% 80%, 50% 100%, 0 80%, 0 0)";
         }
-        if (t <= 750 || t>=1800) {
-            historyshow[0].style.clipPath="polygon(100% 10%, 100% 10%, 100% 90%, 100% 90%)";
-            msg[0].style.display="none";
-        }
-        else {
+        if (historyxy.top>0 && historyxy.bottom<winheight) {
             historyshow[0].style.clipPath="polygon(0 0, 100% 10%, 100% 90%, 0% 100%)";
             msg[0].style.display="block";
         }
+        else {
+            historyshow[0].style.clipPath="polygon(100% 10%, 100% 10%, 100% 90%, 100% 90%)";
+            msg[0].style.display="none";
+        }
     }
 else{
+    clickshow.src="./img/home/hand-click.png"
     if (t >= 100) {
         logo.style.display = "none";
         header[0].style.backgroundColor = "rgba(0,128,128,.9)";
@@ -54,6 +75,13 @@ else{
         header[0].style.justifyContent="space-between";
         header[0].style.backgroundColor = "rgba(0,128,128,.2)";
     }
+    if (historyxy.top>0 && historyxy.bottom<winheight) {
+        msg[0].style.display="block";
+    }
+    else {
+        msg[0].style.display="none";
+    }
 }
+
 }
 
